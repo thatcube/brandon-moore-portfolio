0b4009e019c8a4d5ef630a4b247e52be3258512e
Brandon Moore <brandonmoore@Brandons-MacBook-Pro.local>
2019-10-14 08:32:05 -0400

    support dark mode, project changes

--- web/src/components/project.js
+++ web/src/components/project.js
@@ -1,30 +1,32 @@
-import {format, distanceInWords, differenceInDays} from 'date-fns'
+import { format, distanceInWords, differenceInDays } from 'date-fns'
 import React from 'react'
-import {Link} from 'gatsby'
-import {buildImageObj} from '../lib/helpers'
-import {imageUrlFor} from '../lib/image-url'
+import { Link } from 'gatsby'
+import { buildImageObj } from '../lib/helpers'
+import { imageUrlFor } from '../lib/image-url'
 import BlockContent from './block-content'
 import Container from './container'
 import RoleList from './role-list'
 
 import styles from './project.module.css'
 
-function Project (props) {
-  const {_rawBody, title, categories, mainImage, members, publishedAt, relatedProjects} = props
+function Project(props) {
+  const { _rawBody, title, categories, mainImage, members, publishedAt, relatedProjects } = props
   return (
     <article className={styles.root}>
-      {props.mainImage && mainImage.asset && (
-        <div className={styles.mainImage}>
-          <img
-            src={imageUrlFor(buildImageObj(mainImage))
-              .width(1200)
-              .height(Math.floor((9 / 16) * 1200))
-              .fit('crop')
-              .url()}
-            alt={mainImage.alt}
-          />
-        </div>
-      )}
+      <Container>
+        {props.mainImage && mainImage.asset && (
+          <div className={styles.mainImage}>
+            <img
+              src={imageUrlFor(buildImageObj(mainImage))
+                .width(1200)
+                .height(Math.floor((9 / 16) * 1200))
+                .fit('crop')
+                .url()}
+              alt={mainImage.alt}
+            />
+          </div>
+        )}
+      </Container>
       <Container>
         <div className={styles.grid}>
           <div className={styles.mainContent}>
@@ -59,8 +61,8 @@ function Project (props) {
                       {project.slug ? (
                         <Link to={`/project/${project.slug.current}`}>{project.title}</Link>
                       ) : (
-                        <span>{project.title}</span>
-                      )}
+                          <span>{project.title}</span>
+                        )}
                     </li>
                   ))}
                 </ul>
