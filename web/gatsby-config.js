798f8d44a6082b032b52e0cd0216d92a03b4f042
Brandon Moore <hi@brandonmoore.io>
2020-01-07 13:46:27 -0500

    add manifest plugin

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -19,9 +19,18 @@ module.exports = {
         ...clientConfig.sanity,
         token,
         watchMode: !isProd,
-        overlayDrafts: !isProd && token,
-        icon: 'src/images/favicon.png'
-      }
+        overlayDrafts: !isProd && token
+      },
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Brandon C. Moore - Product Designer`,
+        short_name: `Brandon C. Moore Portfolio`,
+        start_url: `/`,
+        background_color: `#000`,
+        theme_color: `#000`,
+        display: `standalone`,
+        icon: 'src/images/favicon.png',
+      },
     }
   ]
 }
