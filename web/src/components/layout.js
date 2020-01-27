019a1397f9a67f3fe69747ebb97fb435f3dd9610
Brandon Moore <hi@brandonmoore.io>
2020-01-27 13:40:38 -0500

    fix console svg errors, hero style

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -31,7 +31,7 @@ const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => (
             </ul>
             <ThemeToggler>
               {({ theme, toggleTheme }) => (
-                <label for="darkModeToggle" className="input_wrapper">
+                <label htmlFor="darkModeToggle" className="input_wrapper">
                   <span className="input_label">Dark Mode</span>
                   <input
                     id="darkModeToggle"
@@ -41,8 +41,8 @@ const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => (
                     checked={theme === 'dark'}
                   />
                   <div className="input_toggle">
-                    <svg className="is_checked" viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg"><path d="M30 53.9L18.43 42.33a3.32 3.32 0 10-4.7 4.7l13.94 13.94c1.3 1.3 3.4 1.3 4.7 0L67.63 25.7a3.32 3.32 0 10-4.7-4.7L30 53.9z" fill="#FFF" fill-rule="nonzero"/></svg>
-                    <svg className="is_unchecked" viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg"><path d="M61 19a3.3 3.3 0 00-4.7 0L40 35.3 23.7 19a3.3 3.3 0 10-4.7 4.7L35.3 40 19 56.3a3.3 3.3 0 104.7 4.7L40 44.7 56.3 61a3.3 3.3 0 104.7-4.7L44.7 40 61 23.7a3.3 3.3 0 000-4.7z" fill="#FFF" fill-rule="nonzero"/></svg>
+                    <svg className="is_checked" viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg"><path d="M30 53.9L18.43 42.33a3.32 3.32 0 10-4.7 4.7l13.94 13.94c1.3 1.3 3.4 1.3 4.7 0L67.63 25.7a3.32 3.32 0 10-4.7-4.7L30 53.9z" fill="#FFF" fillRule="nonzero"/></svg>
+                    <svg className="is_unchecked" viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg"><path d="M61 19a3.3 3.3 0 00-4.7 0L40 35.3 23.7 19a3.3 3.3 0 10-4.7 4.7L35.3 40 19 56.3a3.3 3.3 0 104.7 4.7L40 44.7 56.3 61a3.3 3.3 0 104.7-4.7L44.7 40 61 23.7a3.3 3.3 0 000-4.7z" fill="#FFF" fillRule="nonzero"/></svg>
                   </div>
                   {' '}
                 </label>
