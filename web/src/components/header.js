ea2efedfd66221303167ef6139ca6cd3c214ef7c
Brandon Moore <brandon-moore@live.com>
2020-02-01 09:09:36 -0500

    semantics, header optimizations

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -14,23 +14,17 @@ const active = {
 
 const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => (
   <Headroom>
-    <div className={styles.root}>
-      <div className={styles.wrapper}>
+    <header className={styles.root}>
+      <nav className={styles.wrapper}>
         <div className={styles.branding}>
           <Link to='/'>
             <svg aria-labelledby="logoTitle" width="40" height="40" viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg">
               <title id="logoTitle">Brandon Moore's portfolio logo</title>
-              <path d="M22 2a20 20 0 110 40 20 20 0 010-40zm0 8a12 12 0 00-12 12c16 4.4 24 4.4 24 0a12 12 0 00-12-12z" fill="currentColor" fillRule="evenodd" /></svg>
+              <path d="M22 2a20 20 0 110 40 20 20 0 010-40zm0 8a12 12 0 00-12 12c16 4.4 24 4.4 24 0a12 12 0 00-12-12z" fill="currentColor" fillRule="evenodd" />
+            </svg>
           </Link>
         </div>
-        <div className={styles.toggleNavButton}>
-          {/* <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}> */}
-          <button className={button} onClick={showNav ? onHideNav : onShowNav}>
-            {/* <Icon symbol='hamburger' /> */}
-            Menu
-      </button>
-        </div>
-        <nav className={cn(styles.nav, showNav && styles.showNav)}>
+        <div className={styles.nav}>
           <ul>
             <li>
               <Link to='/' activeStyle={active} title="Work">Work</Link>
@@ -39,9 +33,9 @@ const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => (
               <Link to='/about' activeStyle={active} title="About">About</Link>
             </li>
           </ul>
-        </nav>
-      </div>
-    </div>
+        </div>
+      </nav>
+    </header>
   </Headroom>
 )
 
