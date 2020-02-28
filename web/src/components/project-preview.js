b171b9c717f9b66ad24f1c5279e7fca3df5a6101
Brandon Moore <brandon-moore@live.com>
2020-02-28 10:52:02 -0500

    redesign layout of projects grid

--- web/src/components/project-preview.js
+++ web/src/components/project-preview.js
@@ -1,16 +1,16 @@
-import {Link} from 'gatsby'
+import { Link } from 'gatsby'
 import React from 'react'
-import {cn, buildImageObj} from '../lib/helpers'
-import {imageUrlFor} from '../lib/image-url'
+import { cn, buildImageObj } from '../lib/helpers'
+import { imageUrlFor } from '../lib/image-url'
 import BlockText from './block-text'
 
 import styles from './project-preview.module.css'
-import {responsiveTitle2} from './typography.module.css'
+import { responsiveTitle2 } from './typography.module.css'
 
-function ProjectPreview (props) {
+function ProjectPreview(props) {
   return (
     <Link className={styles.root} to={`/${props.slug.current}`}>
-      <div className={styles.leadMediaThumb}>
+      <div className={styles.projectThumbnail}>
         {props.mainImage && props.mainImage.asset && (
           <img
             src={imageUrlFor(buildImageObj(props.mainImage))
@@ -22,13 +22,13 @@ function ProjectPreview (props) {
           />
         )}
       </div>
-      <div className={styles.cardBottom}>
-      <h2 className={cn(responsiveTitle2, styles.title)}>{props.title}</h2>
-      {props._rawExcerpt && (
-        <div className={styles.excerpt}>
-          <BlockText className={styles.excerpt} blocks={props._rawExcerpt} />
-        </div>
-      )}
+      <div className={styles.projectDesc}>
+        <h2 className={cn(responsiveTitle2, styles.title)}>{props.title}</h2>
+        {props._rawExcerpt && (
+          <div className={styles.excerpt}>
+            <BlockText className={styles.excerpt} blocks={props._rawExcerpt} />
+          </div>
+        )}
       </div>
     </Link>
   )
