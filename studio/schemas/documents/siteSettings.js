9909d1a15483cee741d349b980d503ff5e5f2f9b
Brandon Moore <brandon-moore@live.com>
2020-03-11 11:18:24 -0400

    add meta image to studio

--- studio/schemas/documents/siteSettings.js
+++ studio/schemas/documents/siteSettings.js
@@ -1,41 +1,46 @@
 export default {
-  name: 'siteSettings',
-  type: 'document',
-  title: 'Site Settings',
+  name: "siteSettings",
+  type: "document",
+  title: "Site Settings",
   __experimental_actions: [
     // 'create',
-    'update',
+    "update",
     // 'delete',
-    'publish'
+    "publish"
   ],
   fields: [
     {
-      name: 'title',
-      type: 'string',
-      title: 'Title'
+      name: "metaImage",
+      title: "Meta Image - keep it 1200x720 I think",
+      type: "figure"
     },
     {
-      name: 'description',
-      type: 'text',
-      title: 'Description',
-      description: 'Describe your portfolio for search engines and social media.'
+      name: "title",
+      type: "string",
+      title: "Title"
     },
     {
-      name: 'keywords',
-      type: 'array',
-      title: 'Keywords',
-      description: 'Add keywords that describes your portfolio.',
-      of: [{type: 'string'}],
+      name: "description",
+      type: "text",
+      title: "Description",
+      description: "Describe your portfolio for search engines and social media."
+    },
+    {
+      name: "keywords",
+      type: "array",
+      title: "Keywords",
+      description: "Add keywords that describes your portfolio.",
+      of: [{ type: "string" }],
       options: {
-        layout: 'tags'
+        layout: "tags"
       }
     },
     {
-      name: 'author',
-      type: 'reference',
-      description: 'Publish an author and set a reference to them here.',
-      title: 'Author',
-      to: [{type: 'person'}]
+      name: "author",
+      type: "reference",
+      description: "Publish an author and set a reference to them here.",
+      title: "Author",
+      to: [{ type: "person" }]
     }
   ]
-}
+};
