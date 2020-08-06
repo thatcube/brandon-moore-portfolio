42754517f63ce0bb21389c7e44908729dd74ab4e
Brandon Moore <brandon-moore@live.com>
2020-08-05 22:07:38 -0400

    update/fix project password screen

--- web/src/@mkitio/gatsby-theme-password-protect/components/PasswordProtect.js
+++ web/src/@mkitio/gatsby-theme-password-protect/components/PasswordProtect.js
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 import { setSessionPassword } from '../utils/utils';
 import Layout from '../../../components/layout'
 import Container from '../../../components/container'
+import { Link } from "gatsby";
 
 const styles = {
     input: {
@@ -26,40 +27,46 @@ const PasswordProtect = () => {
     };
 
     return (
-        <Layout>
-            <Container>
-                <div className="col-1-start col-9-end passwordWrapper fade-zoom">
-                    <form onSubmit={onSubmit}>
-                        <h1 style={{ color: 'var(--color-text-high-contrast)' }}>Password please</h1>
-                        <div className="textBoxContainer">
+        <div className="passwordModal">
+            <Layout>
+                <Container>
+                    <div className="col-1-start col-9-end passwordWrapper fade-zoom">
+                        <Link to="/" className="modalClose">
+                            <svg width="48" height="48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M31.35 16.662a1.162 1.162 0 00-1.645 0L24 22.355l-5.705-5.705a1.162 1.162 0 00-1.645 0 1.162 1.162 0 000 1.645L22.355 24l-5.705 5.705a1.162 1.162 0 000 1.645c.455.455 1.19.455 1.645 0L24 25.645l5.705 5.705c.455.455 1.19.455 1.645 0a1.162 1.162 0 000-1.645L25.645 24l5.705-5.705a1.17 1.17 0 000-1.634z" fill="currentColor" /></svg>
+                        </Link>
 
-                            <input
-                                name="password"
-                                type="password"
-                                id="passwordinput"
-                                value={password}
-                                className="textbox"
-                                onChange={event => setPassword(event.target.value)}
-                                style={styles.input}
-                            />
-                            <label className="textBoxLabel" for="passwordinput">Password</label>
+                        <form onSubmit={onSubmit}>
+                            <h1 style={{ color: 'var(--color-text-high-contrast)' }}>Password please</h1>
+                            <div className="textBoxContainer">
 
-                        </div>
+                                <input
+                                    name="password"
+                                    type="password"
+                                    id="passwordinput"
+                                    value={password}
+                                    className="textbox"
+                                    onChange={event => setPassword(event.target.value)}
+                                    style={styles.input}
+                                />
+                                <label className="textBoxLabel" htmlFor="passwordinput">Password</label>
 
-                        <button
-                            type="submit"
-                            className="button"
-                            style={styles.button}
-                        >
-                            Submit
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="button"
+                                style={styles.button}
+                            >
+                                Submit
                         </button>
-                    </form>
+                        </form>
 
 
 
-                </div>
-            </Container>
-        </Layout>
+                    </div>
+                </Container>
+            </Layout>
+        </div>
     );
 };
 
