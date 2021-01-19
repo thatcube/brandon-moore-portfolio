902cdca54243399b2361bf3b2f4cce1f92f960e3
Brandon Moore <brandon-moore@live.com>
2021-01-19 17:10:49 -0500

    update project header

--- web/src/components/project.js
+++ web/src/components/project.js
@@ -32,18 +32,17 @@ function Project(props) {
       className={styles.root}
     >
       <aside className="project-header">
-        <Container>
+        <div className="project-header-padding">
           <div data-sal="slide-up"
         data-sal-easing="ease"
           data-sal-duration="800"
-          data-sal-delay="0" className="row-1-start col-1-start col-9-end">
+          data-sal-delay="0">
             <h1 className={styles.title}>{title}</h1>
           </div>
           <div data-sal="slide-up"
         data-sal-easing="ease"
           data-sal-duration="1200"
-          data-sal-delay="0" className="row-2-start col-1-start col-9-end"><span  className={styles.shortdesc}>{shortdesc}</span></div>
-        </Container>
+          data-sal-delay="0"><span className={styles.shortdesc}>{shortdesc}</span></div>
         <div data-sal="slide-up"
         data-sal-easing="ease"
           data-sal-duration="800"
@@ -57,6 +56,38 @@ function Project(props) {
                 fluid={mainImage.asset.fluid}
               />
             )}</div>
+
+<div data-sal="slide-up"
+        data-sal-easing="ease"
+          data-sal-duration="800"
+          data-sal-delay="0" className="project-info">
+          
+          <span className="col">
+            <span><svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0 12C0 5.38364 5.38364 0 12 0C18.6164 0 24 5.38364 24 12C24 18.6164 18.6164 24 12 24C5.38364 24 0 18.6164 0 12Z" fill="rgba(var(--color-background-inverted-rgb), 0.2)"></path><circle cx="12" cy="12" r="8" fill="var(--color-text-high-contrast)"></circle><circle cx="12" cy="12" r="5" fill="var(--color-background)"></circle><circle cx="12" cy="12" r="2" fill="var(--color-text-high-contrast)"></circle></svg></span>
+            <span className="project-header-caption">Problem</span>
+            {timeframe}
+          </span>
+          <span className="col">
+            <span><svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0 12C0 5.38364 5.38364 0 12 0C18.6164 0 24 5.38364 24 12C24 18.6164 18.6164 24 12 24C5.38364 24 0 18.6164 0 12Z" fill="rgba(var(--color-background-inverted-rgb), 0.2)"></path><circle cx="12" cy="12" r="8" fill="var(--color-text-high-contrast)"></circle><path d="M8.5 12L11 14.5L15.5 10" stroke="var(--color-background)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></span>
+            <span className="project-header-caption">Solution</span>
+            {when}
+          </span>
+          <span className="col">
+            <span>
+            <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="12" cy="12" r="12" fill="rgba(var(--color-background-inverted-rgb), 0.2)"/>
+  <circle cx="12" cy="12" r="8" fill="var(--color-text-high-contrast)"/>
+  <path d="M12 8c-.546 0-.962.186-1.252.536-.26.315-.414.758-.464 1.295-.054.563.019.99.25 1.295.266.352.741.536 1.466.536.724 0 1.199-.186 1.465-.54.23-.307.303-.733.252-1.291-.05-.545-.204-.988-.465-1.302C12.963 8.183 12.548 8 12 8zm0 4.215c1.366 0 2.308.697 2.89 1.784.341.635-.158 1.325-.884 1.325H9.994c-.726 0-1.225-.69-.885-1.325.583-1.088 1.525-1.784 2.891-1.784z" fill="var(--color-background)"/>
+</svg>
+
+
+
+            </span>
+            <span className="project-header-caption">My Role</span>
+            {role}
+          </span>
+      </div> 
+      </div>
       </aside>
 
       {/* {props.mainImage && mainImage.asset && (
@@ -86,33 +117,7 @@ function Project(props) {
         </span>
       </Container> */}
 
-      <aside className="project-info">
-          
-          <span className="col">
-            <span><svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0 12C0 5.38364 5.38364 0 12 0C18.6164 0 24 5.38364 24 12C24 18.6164 18.6164 24 12 24C5.38364 24 0 18.6164 0 12Z" fill="rgba(var(--color-background-inverted-rgb), 0.2)"></path><circle cx="12" cy="12" r="8" fill="var(--color-text-high-contrast)"></circle><circle cx="12" cy="12" r="5" fill="var(--color-background)"></circle><circle cx="12" cy="12" r="2" fill="var(--color-text-high-contrast)"></circle></svg></span>
-            <span className="project-header-caption">Problem</span>
-            {timeframe}
-          </span>
-          <span className="col">
-            <span><svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0 12C0 5.38364 5.38364 0 12 0C18.6164 0 24 5.38364 24 12C24 18.6164 18.6164 24 12 24C5.38364 24 0 18.6164 0 12Z" fill="rgba(var(--color-background-inverted-rgb), 0.2)"></path><circle cx="12" cy="12" r="8" fill="var(--color-text-high-contrast)"></circle><path d="M8.5 12L11 14.5L15.5 10" stroke="var(--color-background)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></span>
-            <span className="project-header-caption">Solution</span>
-            {when}
-          </span>
-          <span className="col">
-            <span>
-            <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <circle cx="12" cy="12" r="12" fill="rgba(var(--color-background-inverted-rgb), 0.2)"/>
-  <circle cx="12" cy="12" r="8" fill="var(--color-text-high-contrast)"/>
-  <path d="M12 8c-.546 0-.962.186-1.252.536-.26.315-.414.758-.464 1.295-.054.563.019.99.25 1.295.266.352.741.536 1.466.536.724 0 1.199-.186 1.465-.54.23-.307.303-.733.252-1.291-.05-.545-.204-.988-.465-1.302C12.963 8.183 12.548 8 12 8zm0 4.215c1.366 0 2.308.697 2.89 1.784.341.635-.158 1.325-.884 1.325H9.994c-.726 0-1.225-.69-.885-1.325.583-1.088 1.525-1.784 2.891-1.784z" fill="var(--color-background)"/>
-</svg>
-
-
-
-            </span>
-            <span className="project-header-caption">My Role</span>
-            {role}
-          </span>
-      </aside> 
+      
       {_rawBody && <BlockContent blocks={_rawBody || []} />}
       
       {/* <button className="button to-top" onClick={() => scrollTo('#top-of-page')}>Back to Top <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M12.7 8.306l5 5.107c.2.204.3.408.3.715 0 .306-.1.51-.3.715-.4.408-1 .408-1.4 0l-3.3-3.37v7.676a1 1 0 01-1.993.117L11 19.149v-7.676l-3.3 3.37c-.4.408-1 .408-1.4 0-.4-.409-.4-1.022 0-1.43l5-5.107c.4-.408 1-.408 1.4 0zM17 4a1 1 0 01.117 1.993L17 6H7a1 1 0 01-.117-1.993L7 4h10z" fill="currentColor" fill-rule="nonzero"/></svg></button> */}
